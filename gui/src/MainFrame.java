ed66b4b02a238d86080ccf7d551193c92ee1980a
Konrad Reiche <konrad.reiche@fu-berlin.de>
2011-03-28 23:56:26 +0200

    reorganized directory structure

---
+++ gui/src/MainFrame.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+
+public class MainFrame extends JFrame {
+
+	public MainFrame() {
+		this.setSize(500, 400);
+		this.setVisible(true);
+	}
+	
+	public static void main(String args[]) {
+		new MainFrame();
+	}
+	
+}
