3ded6b84b0c99cc1b7a916567628f3471eee313f
Konrad Reiche <konrad.reiche@fu-berlin.de>
2011-04-08 22:17:01 +0200

    removed binaries

--- src/Debug/cpp/AgentSocket.d
+++
@@ -1,4 +0,0 @@
-cpp/AgentSocket.d cpp/AgentSocket.o: ../cpp/AgentSocket.cpp \
- ../cpp/AgentSocket.h
-
-../cpp/AgentSocket.h:
--- src/Debug/cpp/ThreadInfo.d
+++
@@ -1,9 +0,0 @@
-cpp/ThreadInfo.d cpp/ThreadInfo.o: ../cpp/ThreadInfo.cpp \
- ../cpp/ThreadInfo.h /home/konrad/tinyxml/tinyxml.h \
- /home/konrad/tinyxml/tinystr.h
-
-../cpp/ThreadInfo.h:
-
-/home/konrad/tinyxml/tinyxml.h:
-
-/home/konrad/tinyxml/tinystr.h:
