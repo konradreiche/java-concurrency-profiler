546fa391a215d6106f7d95aaba40e4de315727db
Konrad Reiche <konrad.reiche@fu-berlin.de>
2011-04-22 01:46:35 +0200

    added producer consumer pattern example

---
+++ src/java/Examples.java
@@ -0,0 +1,8 @@
+public class Examples {
+
+	public static void main(String[] args) {
+		Drop drop = new Drop();
+		(new Thread(new Producer(drop))).start();
+		(new Thread(new Consumer(drop))).start();
+	}
+}
